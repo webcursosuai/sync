0a7c978b9d6553fd6964b4cacd315d955e0cbd1f
Hans Cristian Jeria Díaz <hansjeria@gmail.com>
2017-03-21 22:52:27 -0300

    remove transactions

--- cli/syncomega.php
+++ cli/syncomega.php
@@ -78,23 +78,17 @@ if($academicids){
 	}
 	
 	foreach ($academicids as $academicid) {
-		$transaction = $DB->start_delegated_transaction();
-		try {
-			// Courses from Omega
-			list($courses, $syncinfo) = sync_getcourses_fromomega($academicid, $syncinfo, $options["debug"]);
-			// Insert the  courses
-			$DB->insert_records("sync_course", $courses);
-			// Users from Omega
-			list($users, $syncinfo) = sync_getusers_fromomega($academicid, $syncinfo, $options["debug"]);
-			// Insert the enrolments
-			$DB->insert_records("sync_enrol", $users);
-			/*mtrace("Error try to insert the enrolments into the database");
-			mtrace("Forcing exit");
-			exit(0);*/
-		} catch (moodle_exception $exception) {
-			$DB->rollback_delegated_transaction($transaction, $exception);
-			return false;
-		}
+		// Courses from Omega
+		list($courses, $syncinfo) = sync_getcourses_fromomega($academicid, $syncinfo, $options["debug"]);
+		// Insert the  courses
+		$DB->insert_records("sync_course", $courses);
+		// Users from Omega
+		list($users, $syncinfo) = sync_getusers_fromomega($academicid, $syncinfo, $options["debug"]);
+		// Insert the enrolments
+		$DB->insert_records("sync_enrol", $users);
+		/*mtrace("Error try to insert the enrolments into the database");
+		mtrace("Forcing exit");
+		exit(0);*/
 	}
 	// insert records in sync_history
 	$historyrecords = array();
@@ -110,7 +104,6 @@ if($academicids){
 		mtrace("Academic Period ".$academic.", Total courses ".$rowinfo["course"].", Total enrol ".$rowinfo["enrol"]."\n");
 	}
 	$DB->insert_records("sync_history", $historyrecords);
-	$DB->commit_delegated_transaction($transaction);
 }else{
 	mtrace("No se encontraron Periodos académicos activos para sincronizar.");
 	if(!$DB->execute("TRUNCATE TABLE {sync_course}")) {
