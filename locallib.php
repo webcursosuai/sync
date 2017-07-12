20ddeeddd9674dbe6a9c40359f689979b07d5934
Sikeze <mipozarski@alumnos.uai.cl>
2017-07-12 11:32:06 -0400

    insertdata->user fix

--- locallib.php
+++ locallib.php
@@ -506,6 +506,7 @@ function sync_generate_metausers($academicids, $options = null){
 		if($user->Email !== "" && $user->Email !== NULL){
 			$insertdata = new stdClass();
 			$academicid = $user->PeriodoAcademicoId;
+			$insertdata->user = ($CFG->sync_emailexplode) ? explode("@", $user->Email)[0] : $user->Email;
 			$generalcoursedata = new stdClass();
 			$generalcoursedata->course = ($insertdata->role == $CFG->sync_teachername) ? $academicid."-PROFESORES" : $academicid."-ALUMNOS";
 			$generalcoursedata->user = $insertdata->user;
