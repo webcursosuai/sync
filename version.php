68d22a3e59c1ecedbe75e77730b3a0dd4cb31a15
webcursosti <34458134+webcursosti@users.noreply.github.com>
2018-10-17 12:47:02 -0300

    Update version.php

--- version.php
+++ version.php
@@ -28,6 +28,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->pluginname = "local_sync";
 $plugin->component = 'local_sync';
-$plugin->version   = '2017041501';
+$plugin->version   = 2017041501;
 $plugin->requires = 2015111600;
 
